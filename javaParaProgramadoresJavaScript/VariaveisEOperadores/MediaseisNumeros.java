9be12c03b9f355364850daaef66a329914084abe
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-09 04:05:42 -0300

    Exemplo de media de 6 numeros

---
+++ javaParaProgramadoresJavaScript/VariaveisEOperadores/MediaseisNumeros.java
@@ -0,0 +1,19 @@
+public class MediaseisNumeros{
+    public static void main(String[] args){
+        int n1 = 10;
+        int n2 = 10;
+        int n3 = 10;
+        int n4 = 10;
+        int n5 = 10;
+        int n6 = 10;
+
+        int resposta;
+
+        resposta = (n1 + n2 + n3 + n4 + n5 + n6)/6;
+        
+        System.out.println("Resultado da media de seis numeros:");
+        System.out.println(resposta);
+        System.out.println("***************************");
+
+    }
+}
