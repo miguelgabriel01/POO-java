ecaf9a323c5d0ca243897db76a1caf643c872442
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-09 04:33:21 -0300

    Envio de exempĺos em java

---
+++ javaParaProgramadoresJavaScript/VariaveisEOperadores/MaiorDoisNumeros.java
@@ -0,0 +1,14 @@
+public class MaiorDoisNumeros{
+    public static void main(String[] args){
+        int numeroA = 10;
+        int numeroB = 10;
+
+        if(numeroA > numeroB){
+            System.out.println("numeroA é maior que numeroB");
+        }else if(numeroB > numeroA){
+            System.out.println("numeroB é maior que numeroA");
+        }else{
+            System.out.println("Os numeros são iguais!");
+        }
+    }
+}
---
+++ javaParaProgramadoresJavaScript/VariaveisEOperadores/VolumeDeUmaCaixa.java
@@ -0,0 +1,13 @@
+public class VolumeDeUmaCaixa{
+    public static void main(String[] args){
+        float base = 6.55f;
+        float altura = 9.44f;
+        float profundidade = 22.6f;
+        float resposta;
+
+        resposta = base * altura * profundidade;
+        System.out.println("Resultado do volume de uma caixa:");
+        System.out.println(resposta);
+        System.out.println("***************************");
+    }
+}
