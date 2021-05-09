54dd49167af70cd79ba35ca6eaaabfeac4ea91fd
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-09 14:06:56 -0300

    Exemplo de validação de variaveis negativas

---
+++ javaParaProgramadoresJavaScript/operadoresLogicos/Negativas.java
@@ -0,0 +1,12 @@
+public class Negativas{
+    public static void main(String[] args){
+        int a = -20;
+        int b = -4;
+
+        if(a < 0 && b < 0){
+            System.out.println("As duas vareaveis são negativas");
+        }else{
+            System.out.println("As duas variaveis não são negativas");
+        }
+    }
+}
