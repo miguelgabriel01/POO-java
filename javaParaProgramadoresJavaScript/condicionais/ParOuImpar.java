d6cc1cbf91d0dac162a8fbb1feb8cf6ed47b98e6
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-09 04:51:44 -0300

    Exemplo de verificação de numeros pares ou impares

---
+++ javaParaProgramadoresJavaScript/condicionais/ParOuImpar.java
@@ -0,0 +1,11 @@
+public class ParOuImpar{
+    public static void main(String[] args){
+        int n = 19;
+
+        if(n %2==0){
+            System.out.println("Numero par");
+        }else if(n %2!=0){
+            System.out.println("Numero é impar");
+        }
+    }
+}
