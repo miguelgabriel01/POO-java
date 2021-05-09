55d9467e9cf3bea13109f4e1582b10c6fd8b979a
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-09 04:22:15 -0300

    Exemplo de volume de um cilindro

---
+++ javaParaProgramadoresJavaScript/VariaveisEOperadores/VolumeCilindro.java
@@ -0,0 +1,13 @@
+public class VolumeCilindro{
+    public static void main(String[] args){
+        int AreaDaBase = 17;
+        int altura = 8;
+        int resposta;
+
+        resposta = AreaDaBase * altura;
+        System.out.println("Resultado do volume de um cilindro:");
+        System.out.println(resposta);
+        System.out.println("***************************");
+
+    }
+}
