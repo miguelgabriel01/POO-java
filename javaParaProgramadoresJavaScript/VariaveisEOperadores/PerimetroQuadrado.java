14ae7cd4235d0d3fcc8c1ef05d00b4afe548586e
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-09 04:18:26 -0300

    Exemplo de perimetro de um quadrado

---
+++ javaParaProgramadoresJavaScript/VariaveisEOperadores/PerimetroQuadrado.java
@@ -0,0 +1,11 @@
+public class PerimetroQuadrado{
+   public static void main(String[] args){
+       int aresta = 5;
+       int resposta;
+
+       resposta = aresta * 4;
+        System.out.println("Resultado do perimetro de um quadrado:");
+        System.out.println(resposta);
+        System.out.println("***************************");
+   }
+}
