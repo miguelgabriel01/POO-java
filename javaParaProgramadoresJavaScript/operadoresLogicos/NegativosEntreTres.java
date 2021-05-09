77d15916ab1b90e08043aebea2b4a5bc794d2abe
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-09 14:14:53 -0300

    Exemplo de validação de numeros negativos

---
+++ javaParaProgramadoresJavaScript/operadoresLogicos/NegativosEntreTres.java
@@ -0,0 +1,13 @@
+public class NegativosEntreTres{
+    public static void main(String[] args){
+        int a = 40;
+        int b = -25;
+        int c = 1;
+
+        if( a < 0 || b < 0 || c < 0){
+            System.out.println("Existe um numero negativo!");
+        }else{
+            System.out.println("Nenhum numero negativo");
+        }
+    }
+}
