54eff1730ab224e2306cbd43bb810ed52690e4cf
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-09 03:54:59 -0300

    Exemplo de verificação do valor de um cubo

---
+++ javaParaProgramadoresJavaScript/VariaveisEOperadores/ValorCubo.java
@@ -0,0 +1,13 @@
+public class ValorCubo{
+
+    public static void main(String[] args){
+        int aresta = 10;
+        int resposta;
+
+        resposta = aresta * aresta * aresta;
+
+        System.out.println("Resultado do valor do cubo:");
+        System.out.println(resposta);
+        System.out.println("***************************");
+    }
+}
