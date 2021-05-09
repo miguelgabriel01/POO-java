41642319f670b057d772c5e77dcd25c139a6ac04
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-09 14:10:19 -0300

    Exemplo de validação de numero

---
+++ javaParaProgramadoresJavaScript/operadoresLogicos/EntreZeroEQuinze.java
@@ -0,0 +1,11 @@
+public class EntreZeroEQuinze{
+    public static void main(String[] args){
+        int n = 6;
+
+        if(n >= 0 && n <=15){
+            System.out.println("A variavel esta entre 0 e 15!");
+        }else{
+            System.out.println("A variavel não esta entre 0 e 15!");
+        }
+    }
+}
