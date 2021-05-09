4f5f1f139de1a86639abbdc336f169edc54672cc
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-09 13:58:22 -0300

    Exemplo de verificação de numeros divisiveis por dois e por três

---
+++ javaParaProgramadoresJavaScript/operadoresLogicos/DivisivelTresEDois.java
@@ -0,0 +1,11 @@
+public class DivisivelTresEDois{
+    public static void main(String[] args){
+        int numero = 10;
+
+        if(numero %2==0 && numero %3==0 ){
+            System.out.println("Numero divisivel por dois e três!");
+        }else{
+            System.out.println("Numero não divisivel por dois e por três!");
+        }
+    }
+}
