ccc2ba9c56850194245319f4f0ff946fa0b2fce1
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-09 13:51:04 -0300

    Exemplo de validação de numero maior

---
+++ javaParaProgramadoresJavaScript/condicionais/Maior.java
@@ -0,0 +1,14 @@
+public class Maior{
+    public static void main(String[] args){
+        int a = 40;
+        int b = 13;
+
+        if(a > b){
+            System.out.println(a);
+        }else if(b > a){
+            System.out.println(b);
+        }else{
+            System.out.println("Numeros iguais!");
+        }
+    }
+}
