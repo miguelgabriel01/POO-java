52ca9afc7b5a6e1176002b274bec5a7696dfd0cf
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-09 03:46:22 -0300

    Exemplo de hello world no Java

---
+++ javaParaProgramadoresJavaScript/JavaHelloWorld.java
@@ -0,0 +1,12 @@
+public class JavaHelloWorld {
+
+	// metodo main eh o ponto de entrada do programa
+	public static void main(String[] args) {
+
+        System.out.println("............");
+		System.out.println("Hello World");
+        System.out.println("............");
+
+	}
+
+}
