0063306e23df4ec6b96cb013b5550554155637c4
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-09 04:00:42 -0300

    Exemplo de calculo de uma area do triangulo isosceles

---
+++ javaParaProgramadoresJavaScript/VariaveisEOperadores/TrianguloIsosceles.java
@@ -0,0 +1,13 @@
+public class TrianguloIsosceles{
+    public static void main(String[] args){
+        int base = 7;
+        int altura = 16;
+        int resposta;
+
+        resposta = (base*altura)/2;
+        System.out.println("Resultado do valor do cubo:");
+        System.out.println(resposta);
+        System.out.println("***************************");
+
+    }
+}
