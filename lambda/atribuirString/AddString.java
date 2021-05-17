6422f5e03975c4120a7b0c00f47419684279b217
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-17 15:22:21 -0300

    Exemplo de lambda com varias comandos

--- lambda/atribuirString/AddString.java
+++ lambda/atribuirString/AddString.java
@@ -1,7 +1,17 @@
 public class AddString{
   public static void main(String[] args){
-    Funcao ColocarSR = valor -> "Sr. " + valor;
-    System.out.println(ColocarSR.gerar("Miguel"));
+    //exemplo com uma unica operação
+    //Funcao ColocarSR = valor -> "Sr. " + valor;
+    //System.out.println(ColocarSR.gerar("Miguel"));
+
+    //multiplas op
+    Funcao ColocarSR = valor ->{
+     String valorComPrefixo = "Sr. " + valor;
+     String valorComPrefixoEPonto = valorComPrefixo + ".";
+     return valorComPrefixoEPonto;
+    };
+
+    System.out.println(ColocarSR.gerar("Miguel gabriel"));
   }
 }
 
