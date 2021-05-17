83d45225f40195e730905ab8eb1dc60e8a3ecbe4
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-17 17:24:40 -0300

    Exemplo de uso de funçao de alta ordem com multiplos resultados

--- lambda/altaordem/AltaOrdem.java
+++ lambda/altaordem/AltaOrdem.java
@@ -1,7 +1,14 @@
 public class AltaOrdem{
   public static void main(String[] args){
     Calculo soma = (a,b) -> a + b;
-    System.out.println(executarOperacao(soma,1,3));
+    Calculo divisao = (a,b) -> a / b;
+    Calculo multiplicacao = (a,b) -> a * b;
+    Calculo subtracao = (a,b) -> a - b;
+
+    System.out.println("Resultado da soma: " + executarOperacao(soma,1,3));
+    System.out.println("Resultado da divisão: " + executarOperacao(divisao,1,3));
+    System.out.println("Resultado da multiplicação: " + executarOperacao(multiplicacao,1,3));
+    System.out.println("Resultado da subtração: " + executarOperacao(subtracao,1,3));
   }
 
   public static int executarOperacao(Calculo calculo, int a, int b){
